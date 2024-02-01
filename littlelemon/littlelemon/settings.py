4da22e928fa6f47431b3b7b74a0f73dd84b37f6a
Eric <developer.ericgitangu@gmail.com>
2024-02-01 15:28:45 -0600

    Migrate changes to mysql instance from sqlite.

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -81,6 +81,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'mysql': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reservations',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+        'USER' : 'bistroadmin',
+        'PASSWORD' : 'Acc3ss87!',
     }
 }
 
